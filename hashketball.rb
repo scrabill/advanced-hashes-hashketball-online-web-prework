51396ee9b8d9685c94bb1eb5e228841597682065
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-10 04:45:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,7 @@ def game_hash
   game_hash = {
     :home => {
       :team_name => "Brooklyn Nets",
-      :colors => [],
+      :colors => ["Black","White"],
       :players => [
         {
         :number => "",
