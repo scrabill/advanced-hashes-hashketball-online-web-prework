a6439494bfdbbb45aeebccfba8b54e5707f459b7
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-10 04:38:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,23 @@
 # Write your code here!
 def game_hash
+
+  game_hash = {
+    :home => {
+      :team_name =>
+      :colors =>
+      :players =>
+
+    },
+    :away => {}
+  }
+
+#   dictionary = {
+#   "hello" => 'hi',
+#   "to, two, too" => '2',
+#   "for, four" => '4',
+#   'be' => 'b',
+#   'you' => 'u',
+#   "at" => "@",
+#   "and" => "&"
+# }
 end
